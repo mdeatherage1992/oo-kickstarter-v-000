a48bbd6e23ad6ef0cd98db7577798552fa4aba8c
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-22 16:42:44 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,13 @@
 class Backer
   attr_accessor :name
-
+  @@backers = []
   def initialize(name)
     @name = name
+  end
+
+  def add_backer(backer)
+    backer = Backer.new
+    @@backers << backer
   end
 
 end
