b7b3c3382e366ae0b745379a9e01f0c6fdffd09b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-22 16:41:32 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,14 @@
 class Project
   attr_accessor :title
-
+  @@backed_projects = []
   def initialize(title)
     @title = title
   end
+
+  def back_project(project)
+    project = Project.new
+    @@backed_projects << project
+  end
+
 
 end
